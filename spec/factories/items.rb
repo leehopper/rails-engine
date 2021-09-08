53d817ea2496f752bb205c6d46b05887142bd8c6
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-08 12:32:38 -0600

    Create item factory and with items trait for merchant

---
+++ spec/factories/items.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :item do
+    name { Faker::Movies::StarWars.vehicle }
+    description { Faker::Movies::StarWars.wookie_sentence }
+    sequence(:unit_price) {|n| n*9 }
+    merchant
+  end
+end
