73685f90608ea945e27f1ebdc4e4f7de24b82363
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-09 15:12:49 -0600

    Build test and routing and controller method for top merchants by revenue

---
+++ app/controllers/api/v1/revenue_controller.rb
@@ -0,0 +1,17 @@
+
+module Api
+  module V1
+    # Controller to return revenue json data
+    class RevenueController < ApplicationController
+      def top_merchants
+        merchants = Merchant.top_merchants_by_revenue(quantity_param[:quantity])
+        render json: MerchantSerializer.new(merchants, is_collection: true)
+      end
+
+      private
+      def quantity_param
+        params.permit(:quantity)
+      end
+    end
+  end
+end
--- spec/requests/api/v1/revenue/top_merchants_request_spec.rb
+++ spec/requests/api/v1/revenue/top_merchants_request_spec.rb
@@ -3,6 +3,44 @@ require 'rails_helper'
 RSpec.describe 'Merchants API' do
   describe 'get all merchants' do
     context 'happy path' do
+      it 'returns a variable number of merchants ordered by revenue' do
+        merchant_1 = create(:merchant, :with_items, item_count: 1)
+        m1_invoice_item = create(:invoice_item, item: merchant_1.items.first, quantity: 10, unit_price: 10)
+        create(:transaction, invoice: m1_invoice_item.invoice)
+
+        merchant_2 = create(:merchant, :with_items, item_count: 1)
+        m2_invoice_item = create(:invoice_item, item: merchant_2.items.first, quantity: 9, unit_price: 10)
+        create(:transaction, invoice: m2_invoice_item.invoice)
+
+        merchant_3 = create(:merchant, :with_items, item_count: 3)
+        m3_invoice_item_1 = create(:invoice_item, item: merchant_3.items.first, quantity: 1, unit_price: 10)
+        m3_invoice_item_2 = create(:invoice_item, item: merchant_3.items.second, quantity: 1, unit_price: 10)
+        m3_invoice_item_3 = create(:invoice_item, item: merchant_3.items.last, quantity: 6, unit_price: 10)
+        create(:transaction, invoice: m3_invoice_item_1.invoice)
+        create(:transaction, invoice: m3_invoice_item_2.invoice)
+        create(:transaction, invoice: m3_invoice_item_3.invoice)
+
+        merchant_4 = create(:merchant, :with_items, item_count: 1)
+        m4_invoice_item = create(:invoice_item, item: merchant_4.items.first, quantity: 5, unit_price: 10)
+        create(:transaction, invoice: m4_invoice_item.invoice)
+
+        merchant_5 = create(:merchant, :with_items, item_count: 1)
+        m5_invoice_item = create(:invoice_item, item: merchant_5.items.first, quantity: 1, unit_price: 10)
+        create(:transaction, invoice: m5_invoice_item.invoice)
+
+        get '/api/v1/revenue/merchants?quantity=5'
+
+        merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+
+        expect(merchants.count).to eq(5)
+
+        expected = [merchant_1, merchant_2, merchant_3, merchant_4, merchant_5]
+
+        expected.each_with_index do |merchant, index|
+          expect(merchants[index][:id]).to eq(merchant.id.to_s)
+          expect(merchants[index][:attributes][:name]).to eq(merchant.name)
+        end
+      end
     end
   end
 end
